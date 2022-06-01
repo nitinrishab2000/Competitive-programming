c036c12fdd2f920d7995bd3e147e4705a95da7cb
nitinrishab2000 <71223285+nitinrishab2000@users.noreply.github.com>
2022-06-02 00:46:43 +0530

    Create INCREAR.java

---
+++ Codechef/CodeChef Starters/41/INCREAR.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+class A{
+	static FastScanner fs = null;
+	public static void main(String[] args) {
+		fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = fs.nextInt();
+		while (t-->0) {
+			int x = fs.nextInt();
+			int y = fs.nextInt();
+			int ans = 0;
+			if(y>=x){
+				ans = y-x;
+			}
+			else{
+				int p = x-y;
+				if(p%2==0){
+					ans = p/2;
+				}
+				else{
+					ans = p/2+2;
+				}
+			}
+			out.println(ans);
+		}
+		out.close();
+	}
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+}
